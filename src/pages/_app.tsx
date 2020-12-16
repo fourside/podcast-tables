54c72180ada881abb3c0c986ccce35b3f7993f9f
fourside <fourside@gmail.com>
2020-12-16 09:56:50 +0900

    loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
-import { NextComponentType } from 'next'
+import { useState } from "react"
+import { NextComponentType } from "next";
 import { AppContext, AppInitialProps, AppProps } from "next/app";
-import '../styles/globals.css'
+import Router from "next/router";
+
+import { Loading } from "../components/Loading";
+import "../styles/globals.css";
+
+Router.events.on('routeChangeStart', () => console.log("start"));
+Router.events.on('routeChangeComplete', () => console.log("end"));
+Router.events.on('routeChangeError', () => console.log("error"));
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false)
+
+  Router.events.on('routeChangeStart', () => setLoading(true));
+  Router.events.on('routeChangeComplete', () => setLoading(false));
+  Router.events.on('routeChangeError', () => setLoading(false));
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
