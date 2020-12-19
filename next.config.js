efea0372a64b388c1ad8d19e49948d12d4ad7abc
fourside <fourside@gmail.com>
2020-12-19 11:02:56 +0900

    firebaseui ja

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const { merge } = require("webpack-merge");
+
 module.exports = {
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
   },
-};
+  webpack: (config) => {
+    return (merge(config, {
+      resolve: {
+        alias: {
+          firebaseui: "firebaseui-ja",
+        }
+      }
+    }))
+  }
+}
