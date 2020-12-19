6e560f4b2e3690b79bb546202131ed1139106bfe
fourside <fourside@gmail.com>
2020-12-19 10:21:41 +0900

    style signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import Router from "next/router";
+import styled from "styled-components";
 
+import Layout from "../components/layout";
 import { useAuth, SignInButton } from "../context/Auth";
 
 const Login: React.FC = () => {
@@ -17,10 +19,27 @@ const Login: React.FC = () => {
   }
 
   return (
-     <div className="container">
-       <SignInButton />
-     </div>
+    <Layout>
+      <Container>
+        <Title>Sign in</Title>
+        <SignInButton />
+      </Container>
+    </Layout>
   )
 }
 
 export default Login;
+
+const Title = styled.h2({
+  marginBottom: "2em",
+  textAlign: "center",
+  color: "#888",
+});
+
+const Container = styled.div({
+  maxWidth: "360px",
+  minHeight: "240px",
+  margin: "3rem auto 6rem",
+  border: "1px solid #eee",
+  borderRadius: "10px",
+});
