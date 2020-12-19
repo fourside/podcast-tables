59bd1edbc3dabea21ce7d582192392d3f5088483
fourside <fourside@gmail.com>
2020-12-19 12:20:38 +0900

    firebase emlator

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,15 @@ import "firebase/auth";
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
+}
+
+if (process.env.NODE_ENV !== "production") {
+  firebase.auth().useEmulator("http://localhost:9099");
 }
 
 export default firebase;
