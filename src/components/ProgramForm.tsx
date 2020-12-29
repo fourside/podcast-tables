22f97115188dbe5fb83b5d404bd3331002c6fe64
fourside <fourside@gmail.com>
2020-12-29 12:41:23 +0900

    form validation

--- src/components/ProgramForm.tsx
+++ src/components/ProgramForm.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { Program } from "../lib/station";
 import { PostParams } from "../lib/client";
 import { decodeHtml, formatProgram } from "../lib/util";
 import { SubmitButton } from "./SubmitButton";
+import { formSchema } from "../lib/formSchema";
 
 type Props = {
   stationId: string;
@@ -12,8 +14,9 @@ type Props = {
   onSubmit: (postParams: PostParams) => Promise<void>;
 };
 export const ProgramForm: React.FC<Props> = ({ stationId, program, onSubmit }) => {
-  const { handleSubmit, register, formState } = useForm<PostParams>({
+  const { handleSubmit, register, formState, errors } = useForm<PostParams>({
     mode: "onBlur",
+    resolver: yupResolver(formSchema),
   });
   const { isSubmitting, isValid } = formState;
 
@@ -23,17 +26,61 @@ export const ProgramForm: React.FC<Props> = ({ stationId, program, onSubmit }) =
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
-        <Input type="text" width={20} readOnly={true} name="stationId" value={stationId} ref={register} />
-        <Input type="text" width={77} readOnly={true} name="title" value={formatted.title} ref={register} />
+        <Input
+          type="text"
+          width={20}
+          readOnly={true}
+          name="stationId"
+          value={stationId}
+          ref={register}
+          hasError={!!errors.stationId}
+        />
+        <Input
+          type="text"
+          width={77}
+          readOnly={true}
+          name="title"
+          value={formatted.title}
+          ref={register}
+          hasError={!!errors.title}
+        />
       </FormControl>
+      <ErrorMessage>{errors.stationId?.message}</ErrorMessage>
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
       <FormControl>
-        <Input type="text" readOnly={true} name="personality" value={formatted.personality} ref={register} />
+        <Input
+          type="text"
+          readOnly={true}
+          name="personality"
+          value={formatted.personality}
+          ref={register}
+          hasError={!!errors.personality}
+        />
       </FormControl>
+      <ErrorMessage>{errors.personality?.message}</ErrorMessage>
       <FormControl>
-        <Input type="text" width={40} readOnly={true} name="fromTime" value={formatted.from} ref={register} />
-        <Input type="text" width={15} readOnly={true} name="duration" value={formatted.duration} ref={register} />
+        <Input
+          type="text"
+          width={40}
+          readOnly={true}
+          name="fromTime"
+          value={formatted.from}
+          ref={register}
+          hasError={!!errors.fromTime}
+        />
+        <Input
+          type="text"
+          width={15}
+          readOnly={true}
+          name="duration"
+          value={formatted.duration}
+          ref={register}
+          hasError={!!errors.duration}
+        />
         <EmptySpan width={40} />
       </FormControl>
+      <ErrorMessage>{errors.fromTime?.message}</ErrorMessage>
+      <ErrorMessage>{errors.duration?.message}</ErrorMessage>
       <FormControl>
         <Info dangerouslySetInnerHTML={{ __html: infoHtml }} />
       </FormControl>
@@ -58,7 +105,14 @@ const FormControl = styled.div({
   justifyContent: "space-between",
 });
 
-const Input = styled.input(
+const ErrorMessage = styled.div({
+  color: "#d00",
+  fontSize: "small",
+  marginTop: "-8px",
+  paddingLeft: "8px",
+});
+
+const _Input = styled.input(
   {
     border: "1px solid #eee",
     borderRadius: "10px",
@@ -76,6 +130,17 @@ const Input = styled.input(
     };
   }
 );
+
+const Input = styled(_Input)({}, (props: { hasError: boolean }) => {
+  if (props.hasError) {
+    return {
+      border: "1px solid #f22",
+    };
+  }
+  return {
+    border: "1px solid #eee",
+  };
+});
 
 const EmptySpan = styled.span({}, (props: { width: number }) => {
   return { width: `${props.width}%` };
