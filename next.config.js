44c4eb9a2fd9caf2c951bbace1a737addeb138a0
fourside <fourside@gmail.com>
2021-01-05 00:18:18 +0900

    prevent from post request except writable user

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     API_ENDPOINT: process.env.API_ENDPOINT,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    WRITABLE_USER_MAIL_ADDRESS: process.env.WRITABLE_USER_MAIL_ADDRESS,
   },
   webpack: (config) => {
     return (merge(config, {
