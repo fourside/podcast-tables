b8843227ed93bcab8de2d1fcf00281e7946759bf
fourside <fourside@gmail.com>
2021-12-03 09:45:25 +0900

    rename and wrap simple component

--- src/components/program-columns.tsx
+++ src/components/program-columns.tsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 
 import { Program, ProgramPerDate } from "../lib/station";
 import { Menu } from "./menu";
-import { ProgramColumn } from "./program-column";
 import { Modal } from "./modal";
-import { ProgramForm } from "./form/ProgramForm";
+import { ProgramForm } from "./form/program-form";
 import { PostParams, postProgram } from "../lib/client";
 import { unformatPostParams } from "../lib/util";
 import { useToast } from "../context/Toast";
 import { useAuth } from "../context/Auth";
+import { diffDateFrom, formatMonthDay } from "../lib/day";
+import { columnId } from "../lib/util";
+import { ProgramCard } from "./program-card";
 
 type Props = {
   stationId: string;
@@ -90,4 +92,44 @@ const ColumnContainer = styled.div({
 const SideContainer = styled.div({
   width: "10%",
   paddingRight: "16px",
+});
+
+
+type ProgramColumnProps = {
+  programPerDate: ProgramPerDate;
+  onClick: (program: Program) => void;
+};
+const ProgramColumn: React.FC<ProgramColumnProps> = ({ programPerDate, onClick }) => {
+  const index = diffDateFrom(programPerDate.date);
+  const idAttribute = columnId(index);
+  const date = formatMonthDay(programPerDate.date);
+
+  return (
+    <Column key={programPerDate.date} id={idAttribute}>
+      <Date>{date}</Date>
+      {programPerDate.programs.map((program) => (
+        <ProgramCard program={program} key={program.id} onClick={onClick} />
+      ))}
+    </Column>
+  );
+};
+
+const Column = styled.div({
+  borderRadius: "10px",
+  border: "1px #eee solid",
+  margin: "8px",
+  boxShadow: "4px 4px 12px 2px rgba(0,0,0,0.1)",
+  width: "calc(100% / 4)",
+  display: "flex",
+  flexDirection: "column",
+  flexShrink: 0,
+  gap: "4px",
+  padding: "8px",
+  scrollSnapAlign: "center",
+});
+
+const Date = styled.h3({
+  margin: "8px",
+  paddingLeft: "2px",
+  whiteSpace: "nowrap",
 });
