835b54353d838b744badc2838386cf963ace77f3
fourside <fourside@gmail.com>
2021-12-22 10:00:05 +0900

    wrap menu buttons into one component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 import { diffDateFrom, formatMonthDate } from "../lib/day";
-import { LinkButton } from "./link-button";
 import { Clock } from "./clock";
 import { columnId } from "../lib/util";
+import { VFC } from "react";
 
 type Props = {
   title: string;
@@ -14,12 +14,9 @@ export const Menu: React.FC<Props> = ({ title, dateList }) => {
     <StickyMenu>
       <Title>{title}</Title>
       <Clock />
-      {dateList.map((date) => {
-        const label = formatMonthDate(date);
-        const index = diffDateFrom(date);
-        const idAttribute = columnId(index);
-        return <LinkButton label={label} href={`#${idAttribute}`} key={date} />;
-      })}
+      {dateList.map((date) => (
+        <DateButton key={date} date={date} />
+      ))}
     </StickyMenu>
   );
 };
@@ -35,4 +32,24 @@ const StickyMenu = styled.div({
 
 const Title = styled.h2({
   color: "#888",
+});
+
+type DateButtonProps = {
+  date: string;
+};
+
+const DateButton: VFC<DateButtonProps> = (props) => {
+  const label = formatMonthDate(props.date);
+  const index = diffDateFrom(props.date);
+  const idAttribute = columnId(index);
+  return <_Button href={`#${idAttribute}`}>{label}</_Button>;
+};
+
+const _Button = styled.a({
+  display: "block",
+  backgroundColor: "transparent",
+  border: "1px solid #ccc",
+  borderRadius: "8px",
+  padding: "4px 12px",
+  color: "#666",
 });
