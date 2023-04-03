21d734724950ba4819a8982e9957f24da7d24b3d
fourside <fourside@gmail.com>
2023-04-03 09:14:53 +0900

    remove comments

--- src/components/form/error-form-message.tsx
+++ src/components/form/error-form-message.tsx
@@ -11,10 +11,3 @@ export const ErrorFormMessage: FC<Props> = ({ message }) => {
 
   return <div className="text-red-700 text-sm">{message}</div>;
 };
-
-// const ErrorMessage = styled.div({
-//   color: "#d00",
-//   fontSize: "small",
-//   marginTop: "-8px",
-//   paddingLeft: "8px",
-// });
