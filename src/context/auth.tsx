c0446eaa61b79a0e45e6d571ebfa0a846d5014d1
fourside <fourside@gmail.com>
2023-04-04 09:18:58 +0900

    signin button

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,6 +1,5 @@
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
-import { createContext, useContext, useEffect, useState } from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { GoogleAuthProvider, User, getAuth, onAuthStateChanged, signInWithRedirect } from "firebase/auth";
+import { FC, createContext, useContext, useEffect, useState } from "react";
 import { initializeFirebaseApp } from "../lib/firebase";
 
 export type FirebaseUser = User | null | undefined;
@@ -22,13 +21,14 @@ type AuthContextProps = AuthedProps | UnauthedProps | unknownAuthProps;
 
 const AuthContext = createContext<AuthContextProps>({ authState: "unknown" });
 
-const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: FC = ({ children }) => {
   const [firebaseAuthState, setFirebaseAuthState] = useState<AuthContextProps>({ authState: "unknown" });
 
   useEffect(() => {
     initializeFirebaseApp();
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("onAuthStateChanged", user);
       if (user === null) {
         setFirebaseAuthState({ authState: "fail" });
       } else {
@@ -47,12 +47,6 @@ export const useAuth: () => AuthContextProps = () => {
   return useContext(AuthContext);
 };
 
-const uiConfig = {
-  signInFlow: "redirect",
-  signInOptions: [GoogleAuthProvider.PROVIDER_ID],
-};
-
-export const SignInButton: React.FC = () => {
-  const auth = getAuth();
-  return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />;
-};
+export function signIn(): Promise<void> {
+  return signInWithRedirect(getAuth(), new GoogleAuthProvider());
+}
